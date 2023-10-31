58ed27ddaf7447995986425e1f7fdeaf45ee0c35
BeLieVer031 <sandyrajak031@gmail.com>
2023-10-31 20:12:11 +0530

    data file added

---
+++ version1/utils/data.js
@@ -0,0 +1,12 @@
+const themes = {
+    theme1: "#788CDE",
+    theme2: "#BC7ABC",
+    theme3: "#A0CBF1",
+    theme4: "#D6BDE7",
+    theme5: "#4AA079",
+    theme6: "#479E98",
+    theme7: "#9AD2BA",
+    theme8: "#8BD3CE",
+    theme9: "#D6BDE7",
+    theme10: "#F5B6C2",
+}
